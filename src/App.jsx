48fc1f8313d395fbb01e3252ff3206b3e05f36d7
pnguyen121 <phillynguyen17@gmail.com>
2023-03-05 00:01:35 -0500

    setting up api call state and functions for a quote, saving progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import { Route } from "react-router-dom";
+
+import Quote from '../componenets/Quote/Quote'
 
 function App() {
+
+  const [quote, setQuote] = useState('')
+
+
+  useEffect(() => {
+
+    const quoteFromApi = `https://healthruwords.p.rapidapi.com/v1/quotes/?t=Wisdom&maxR=1&size=medium&id=731`
+    console.log('useEffect ran')
+
+    async function makeApiCall(){
+      try{
+        const responseJson = await fetch(quoteFromApi);
+        console.log(responseJson, 'responseJson here <--')
+
+        const data = await responseJson.json()
+        console.log(data, 'data from the api')
+
+      } catch(err){
+        console.log(err)
+      }
+    }
+
+
+  })
+
+
   return (
     <div className="container max-w-full h-screen">
       <h1 className="bg-red-500 w-screen">MOTIVATIONAL QUOTE PROJECT</h1>
