645da1252a876d6d9e1d69b7f6823cf234ca370e
pnguyen121 <phillynguyen17@gmail.com>
2023-03-10 14:58:20 -0500

    got a new quote to appear on button click

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import Quote from "./Components/Quote/Quote";
 function App() {
   const [quote, setQuote] = useState("");
 
+  // Link from https://rapidapi.com/HealThruWords/api/universal-inspirational-quotes
+  const quoteFromApi = `https://type.fit/api/quotes`;
+  console.log("useEffect ran");
+
+  // variable to get a random number to get a random quote between 1 - 1600
+  let randomNum = Math.floor(Math.random() * 1600) + 1;
+
   useEffect(() => {
-    // variable to get a random number to get a random quote between 1 - 1600
-    let randomNum = Math.floor(Math.random() * 1600) + 1;
-
-    // Link from https://rapidapi.com/HealThruWords/api/universal-inspirational-quotes
-    const quoteFromApi = `https://type.fit/api/quotes`;
-    console.log("useEffect ran");
-
     async function makeApiCall() {
       try {
         const responseJson = await fetch(quoteFromApi);
@@ -34,12 +34,34 @@ function App() {
     makeApiCall();
   }, []);
 
+
+  
+  // onClick function
+  async function newQuoteClick(){
+    console.log('button clicked')
+    try {
+      const responseJson = await fetch(quoteFromApi);
+      console.log(responseJson, "responseJson here <--");
+
+      const data = await responseJson.json();
+      console.log(data, "data from the api");
+      console.log(randomNum, "randomNum");
+
+      // Set the quote taken from the API to state called quote
+      setQuote(data[randomNum]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="container max-w-full h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <h1 className="w-screen text-center pt-5">KEEP PUSHING</h1>
-      <div className="flex grid-rows-1 justify-items-center justify-center items-center w-screen max-w-full h-5/6">
+      <div className="flex flex-col grid-rows-1 justify-items-center justify-center items-center w-screen max-w-full h-5/6">
         <Quote quoteData={quote} />
-
+        <button onClick={() => newQuoteClick()} class="bg-white hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-8">
+          New Quote
+        </button>
       </div>
     </div>
   );
--- src/Components/Quote/Quote.jsx
+++ src/Components/Quote/Quote.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Quote({ quoteData }) {
   return (
-    <div class="max-w-sm h-1/4 rounded overflow-hidden shadow-black shadow-lg bg-white ">
+    <div class="max-w-sm h-fit rounded overflow-hidden shadow-black shadow-lg bg-white ">
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">Quote</div>
         <p class="text-gray-700 text-base mt-5">{quoteData.text} </p>
